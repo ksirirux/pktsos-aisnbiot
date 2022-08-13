97330e1d82489b389f36d6badaaffb3618a61079
ksirirux <ksirirux1@gmail.com>
2022-08-13 16:13:08 +0700

    add pio

---
+++ .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/ZAINCOMPUTER/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/ZAINCOMPUTER/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
